cf57fab0ecfe1c67d8504923561110a436b9c132
Xqhare <xqhare@web.de>
2026-03-12 20:17:47 +0100

    add(error): basic Error Enum

---
+++ src/error.rs
@@ -0,0 +1,26 @@
+pub type LasaResult<T> = std::result::Result<T, LasaError>;
+
+#[derive(Debug)]
+pub enum LasaError {
+    EnvironmentError(String),
+    SettingProcessPriority(String),
+    Generic(String),
+    CommandExecution(String),
+    Parsing(String),
+    DataStorage(String),
+}
+
+impl std::fmt::Display for LasaError {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        match self {
+            LasaError::EnvironmentError(err) => write!(f, "Environment error: {}", err),
+            LasaError::SettingProcessPriority(err) => write!(f, "Process priority error: {}", err),
+            LasaError::Generic(err) => write!(f, "Error: {}", err),
+            LasaError::CommandExecution(err) => write!(f, "Command execution error: {}", err),
+            LasaError::Parsing(err) => write!(f, "Parsing error: {}", err),
+            LasaError::DataStorage(err) => write!(f, "Data storage error: {}", err),
+        }
+    }
+}
+
+impl std::error::Error for LasaError {}
